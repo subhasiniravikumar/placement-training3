1e841fcb7d8c66c31d58f329dc535ab2e8a5a372
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2026-01-01 23:33:21 +0530

    Add LRUCache class with get and put methods
    Implement LRU Cache using OrderedDict with basic operations.

---
+++ 1.1.2026/LRU_cache.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+cache = LRUCache(2)
+cache.put(1, "A")
+cache.put(2, "B")
+print(cache.get(1))
+cache.put(3, "C")
+print(cache.cache)
